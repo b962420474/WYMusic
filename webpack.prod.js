bba82613d4e181cc23532b7e4178fdab617a73cc
xuexiang.yang <xuexiang.yang@iservernetwork.com>
2021-05-10 12:17:14 +0800

    修复eslint风格

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,20 @@
-const { merge } = require("webpack-merge")
-const copyWebpackPlugin = require("copy-webpack-plugin")
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { merge } = require('webpack-merge')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
-process.env.NODE_ENV = "production"
-const baseConfig = require("./webpack.common.js")
+process.env.NODE_ENV = 'production'
+const baseConfig = require('./webpack.common.js')
 
 const prodConfig = merge(baseConfig, {
   plugins: [
-    new copyWebpackPlugin([
-        {
-            from: path.resolve(__dirname, 'public'),
-            to: path.resolve(__dirname, 'dist/public')
-        }
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'public'),
+        to: path.resolve(__dirname, 'dist/public')
+      }
     ]),
     new CleanWebpackPlugin()
-    ]
+  ]
 })
 
 module.exports = prodConfig
